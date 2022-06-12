a37e40dfef1532a11f0acadba4f6129852904ee3
votm7 <votm777@naver.com>
2022-06-12 17:59:30 +0900

    Jar Build Error Fix

--- src/main/java/com/shortcutweb/component/GeoLiteComponent.java
+++ src/main/java/com/shortcutweb/component/GeoLiteComponent.java
@@ -4,15 +4,19 @@ import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 @Component
 @Slf4j
@@ -24,7 +28,11 @@ public class GeoLiteComponent {
     @PostConstruct
     void init() {
         try {
-            File file = resourceLoader.getResource("classpath:static/geo/GeoLite2-City.mmdb").getFile();
+            ClassPathResource classPathResource = new ClassPathResource("static/geo/GeoLite2-City.mmdb");
+            InputStream inputStream = classPathResource.getInputStream();
+            File file = File.createTempFile("GeoLite2-City",".mmdb");
+            Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            file.deleteOnExit();
             reader = new DatabaseReader.Builder(file).build();
         } catch (IOException e) {
             log.error("static/geo/GeoLite2-City.mmdb Not Found");
